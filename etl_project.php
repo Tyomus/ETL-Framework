006a2e205d3875308775c0ad5e5b2b0c80afee9c
Daniel Sotus <141133080+Tyomus@users.noreply.github.com>
2025-09-30 18:49:13 +0200

    Add files via upload

---
+++ etl_project.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ETL-Project</title>
+  </head>
+  <body>
+    <main>
+        <h1>A showchase of the price changes of Gold USD/oz and BTC in USD($) overtime.</h1>  
+            <?php include("showcase.html"); ?>
+      </main>
+  </body>
+</html>
---
+++ showcase.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sqlalchemy import create_engine
+
+engine = create_engine("mariadb+pymysql://tyomus:@localhost/etl_project")
+sql = """
+SELECT created_at, name, price FROM assets WHERE name IN ('Gold','Bitcoin') ORDER BY id;
+"""
+df = pd.read_sql(sql, engine)
+
+df_pivot = df.pivot(index='created_at', columns='name', values='price')
+
+# 3a. Dual-axis plot
+fig, ax1 = plt.subplots(figsize=(10,5))
+ax1.plot(df_pivot.index, df_pivot['Gold'], color='gold', label='Gold')
+ax1.set_ylabel("Gold Price (USD/oz)", color='gold')
+
+ax2 = ax1.twinx()
+ax2.plot(df_pivot.index, df_pivot['Bitcoin'], color='darkorange', label='Bitcoin')
+ax2.set_ylabel("Bitcoin Price (USD)", color='darkorange')
+
+fig.write_html("gold_bitcoin_dualaxis.html", include_plotlyjs="cdn")
